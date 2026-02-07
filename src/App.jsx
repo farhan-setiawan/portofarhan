2468da7163c6711c1ce62214ca30bc32968b5667
Farhan Setiawan <farhanervs@gmail.com>
2026-02-07 16:16:54 +0000

    BERHASIL

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <h1>CI/CD GitHub Pipeline + Self Hosted Runners</h1>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -26,7 +27,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more and this is a test
+        CI/CD Pipeline GitHub Self-Hosted Runners Vite React App
       </p>
     </>
   )
