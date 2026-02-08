3bf89a917b915d0a2e3c2e7e98ea21daceeb0668
Farhan Setiawan <farhanervs@gmail.com>
2026-02-08 01:15:03 +0000

    my portfolio1

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [
     react(),
     tailwindcss(),
