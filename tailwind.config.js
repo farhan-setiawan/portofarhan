db85473474b57e640b842e7b9c48eab3314c4b18
Farhan Setiawan <farhanervs@gmail.com>
2026-02-08 01:28:43 +0000

    my portfolio3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -9,4 +9,3 @@ export default {
   },
   plugins: [],
 }
-
